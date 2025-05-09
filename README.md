82206818236c0735923020ff8e6e81b3e5571417
MORTEZA-ALIZADA <111222586+MORTEZA-KING-007@users.noreply.github.com>
2025-05-09 10:37:53 +0430

    Add files via upload

---
+++ README.md
@@ -0,0 +1,35 @@
+#### 😱 FACEBOOK CRACKING TOOLS FOR LINUX AND WINDOWS (BOTH SUPPORTED)
+##### 💎 AUTHOR : MORTEZA-ALIZADA
+##### 💎 GITHUB : MORTEZA-KING-007
+##### 💎 TELEGRAM : @black-mafia-007
+
+##### WHAT'S NEW IN THIS UPDATE ?
+* 🎃 ADDED MORE FAKE USER-AGENT (1500)
+* 🎃 NO IP BLOCK
+* 🎃 SUPPORT MORE COUNTRIES
+* 🎃 AFGHANISTAN
+* 🎃 PAKISTAN
+* 🎃 INDIA
+* 🎃 ISRAEL (FUCK 4 IT)
+* 🎃 AND MORE COUNTRIES
+
+### 🎃 INSTALLATION 
+```
+apt update --yes && apt upgrade --yes
+```
+```
+apt install git figlet espeak python --yes
+```
+```
+git clone https://github.com/MORTEZA-KING-007/FACEBOOK_CRACK
+```
+```
+cd FACEBOOK_CRACK
+```
+
+```
+bash setup.sh
+```
+```
+python *.py
+```
---
+++ setup.sh
@@ -0,0 +1,41 @@
+echo -e '\033[37m[ + ] FACEBOOK CRACKING TOOL BY MORTEZA ALIZADA.......'
+xdg-open https://t.me/black_mafia_007
+
+sleep 5
+
+clear
+
+apt install figlet -y
+apt install espeak -y
+
+clear
+echo -e '\033[32m'
+figlet SETUP
+echo -e '''
+\033[1;32m╔\033[1;32m𓆩M𓆪\033[1;32m══════════════════[\033[37;41m𓆩 𝐌𝐎𝐑𝐓𝐄𝐙𝐀 𓆪\033[0m\033[1;32m]════════════════\033[1;32m𓆩M𓆪\033[1;32m╗
+\033[1;32m│\033[1;37m☞  \033[1;32mAUTHER     \033[1;37m➟   \033[1;32mMORTEZA ALIZADA                    \033[1;32m│
+\033[1;32m│\033[1;37m☞  \033[1;32mFACEBOOK   \033[1;37m➟   \033[1;32mMORTAZA ALIZADA                    \033[1;32m│
+\033[1;32m│\033[1;37m☞  \033[1;32mGITHUB    \033[1;37m ➟  \033[1;32m MORTEZA-KING-007                  \033[1;32m │
+\033[1;32m│\033[1;37m☞  \033[1;32mVERSION   \033[1;37m ➟   \033[1;32m1.0                            \033[1;32m    │
+\033[1;32m│\033[1;37m☞  \033[1;32mSTATUS    \033[1;37m ➟   \033[1;32mFREE                           \033[1;32m    │
+\033[1;32m╚\033[1;32m𓆩M𓆪\033[1;32m═════\033[41m\033[1;37m[ 𓆩 𝐈𝐅 𝐘𝐎𝐔𝐑 𝐁𝐀𝐃,𝐒𝐎 𝐈 𝐀𝐌 𝐘𝐎𝐔𝐑 𝐃𝐀𝐃𓆪 ]\033[0m\033[1;32m═══════\033[1;32m𓆩M𓆪\033[1;32m╝
+==================================================
+'''
+
+echo -e '\033[37m[ + ] All packages in this project is safe no trojan..........'
+echo -e '\033[37m[ + ] Enter or Press (y/Y) to continue.........'
+
+# TOOL NEED THIS PERMMISION TO MAKE MORTEZA-CP/OK.txt FILE IN /SDCARD
+termux-setup-storage
+
+espeak -s 200 "Installing requirements"
+echo -e '\033[37m[ + ] Installing libraries and other packages.......'
+
+apt install python -y
+pip install requests bs4 colorama pyfiglet psutil
+
+espeak -s 200 "Welcome to facebook crack tool"
+espeak -s 200 "subscribe my telegram channel friends for more VIP tool"
+
+echo -e '\033[37m[ + ] Tool is ready to use enjoy.......'
+
