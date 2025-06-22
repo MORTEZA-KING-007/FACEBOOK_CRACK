1898b60e5e0fe296ff0d30a327c2a6709c9c6098
MORTEZA-ALIZADA <111222586+MORTEZA-KING-007@users.noreply.github.com>
2025-06-22 16:37:59 +0430

    Add files via upload

---
+++ file.py
@@ -0,0 +1,12 @@
+import os
+os.system('clear')
+print("""Because of no support tool is off for a while SUBSCRIBE my \ntelegram and youtube channel friends....
+\n
+YOUTUBE 
+https://www.youtube.com/@black_mafia_007_tmx
+\n
+TELEGRAM
+https://t.me/black_mafia_007
+
+
+""")
